6bbdc7614d6f1636ff260a246166551fb108b07e
Steven Barboza <sbarboza@grupoforco.com>
2021-10-04 00:37:04 -0600

    Ejercicios vistos en clase: Encapsulamiento y Constructores.

--- src/com/ucreativa/oop/presupuesto/Main.java
+++ src/com/ucreativa/oop/presupuesto/Main.java
@@ -1,31 +1,70 @@
 package com.ucreativa.oop.presupuesto;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.ucreativa.oop.presupuesto.entidades.Gasto;
+import com.ucreativa.oop.presupuesto.entidades.RegistroGastos;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Mi nuevo main");
+        /*
+        Gasto gastoLuz = new Gasto(
+        "Luz",
+        "Colones",
+        "Servicios",
+        22000);
 
-        Gasto gastoLuz = new Gasto();
-        gastoLuz.nombre = "Luz";
-        gastoLuz.moneda = "Colones";
-        gastoLuz.monto = 22000;
-        //System.out.println(gastoLuz.nombre + " -> " + gastoLuz.monto);
+        Gasto gastoInternet = new Gasto(
+        "Internet",
+        "Colones",
+        "Servicios",
+        30000);
+*/
 
-        Gasto gastoInternet = new Gasto();
-        gastoInternet.nombre = "Internet";
-        gastoInternet.moneda = "Colones";
-        gastoInternet.monto = 30000;
+        System.out.println("Sistema Registro de Gastos");
+        Scanner consola = new Scanner(System.in);
 
         RegistroGastos registro = new RegistroGastos();
-        registro.gastos = new ArrayList<>();
-        registro.gastos.add(gastoLuz);
-        registro.gastos.add(gastoInternet);
 
-        for (int i = 0; i < registro.gastos.size(); i++) {
-            System.out.println(registro.gastos.get(i).nombre);
+    boolean siga = true;
+    while(siga){
+        System.out.println("Digite el nombre de su gasto:");
+        String nombre = consola.nextLine();
+
+        System.out.println("Digite la moneda:");
+        String moneda = consola.nextLine();
+
+        System.out.println("Digite la categoria de su gasto:");
+        String categoria = consola.nextLine();
+
+        System.out.println("Digite el monto de su gasto:");
+        String montoStr = consola.nextLine();
+        int monto = Integer.parseInt(montoStr);
+
+        Gasto nuevoGasto = new Gasto(nombre,moneda,categoria,monto);
+
+        registro.addGastos(nuevoGasto);
+
+        for (Gasto g : registro.getGastos()){
+            System.out.println(g.getNombre());
         }
+
+        System.out.println("Quiere seguir?('s')");
+        siga = consola.nextLine().equals("s");
+    }
+
+
+        /*
+        for (int i = 0; i < registro.getGastos().size(); i++) {
+            System.out.println(registro.getGastos().get(i).getNombre());
+        }
+        */
+
+
+    /* Lambda
+        registro.getGastos().stream().map(Gasto::getNombre).forEach(System.out::println);
+
+     */
     }
 }
---
+++ src/com/ucreativa/oop/presupuesto/entidades/RegistroGastos.java
@@ -0,0 +1,20 @@
+package com.ucreativa.oop.presupuesto.entidades;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegistroGastos {
+    List<Gasto> gastos;
+
+    public RegistroGastos(){
+        this.gastos = new ArrayList<>();
+    }
+
+    public List<Gasto> getGastos() {
+        return this.gastos;
+    }
+
+    public void addGastos(Gasto item){
+        this.gastos.add(item);
+    }
+}
